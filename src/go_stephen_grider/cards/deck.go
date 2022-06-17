dccd81b60d651276b4a6e06af483bf94aedb3d0d
Aimin Tang <aimin.tang@gmail.com>
2022-06-17 17:04:56 -0500

    cards

---
+++ src/go_stephen_grider/cards/deck.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	suits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	values := []string{"Ace", "Two", "Three"}
+
+	for _, suit := range suits {
+		for _, value := range values {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func (d deck) deal(handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	cards := []string(d)
+	return strings.Join(cards, ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bytes), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		new_idx := r.Intn(len(d) - 1)
+		d[i], d[new_idx] = d[new_idx], d[i]
+	}
+}
